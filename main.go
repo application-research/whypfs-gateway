4abbe7bb29b0a21325510ad5aa5b35ee737b1ed0
alvin-reyes <me@areyes.onl>
2022-12-01 18:25:51 -0500

    added http gateway for dir and files

--- main.go
+++ main.go
@@ -4,16 +4,28 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	whypfs "github.com/application-research/whypfs-core"
+	"github.com/gabriel-vasile/mimetype"
 	cid2 "github.com/ipfs/go-cid"
+	mdagipld "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-unixfs"
+	uio "github.com/ipfs/go-unixfs/io"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/xerrors"
 	"io"
+	"net/http"
 	_ "net/http"
+	"net/url"
 	"os"
 	"os/signal"
+	gopath "path"
+	"strings"
 	"syscall"
+	"time"
 	"whypfs-gateway/gateway"
 )
 
@@ -66,6 +78,8 @@ func GatewayRoutersConfig() {
 	}
 
 	// Routes
+	//e.GET("/gw/:path", OriginalGatewayHandler)
+	e.GET("/gw//ipfs/:path", GatewayResolverCheckHandler)
 	e.GET("/gw/:path", GatewayResolverCheckHandler)
 	e.GET("/gw/dir/:path", GatewayDirResolverCheckHandler)
 	e.GET("/gw/file/:path", GatewayFileResolverCheckHandler)
@@ -142,31 +156,115 @@ func GatewayFileResolverCheckHandler(c echo.Context) error {
 
 // It takes a request, and forwards it to the gateway
 func GatewayResolverCheckHandler(c echo.Context) error {
+	ctx := c.Request().Context()
 	p := c.Param("path")
 	req := c.Request().Clone(c.Request().Context())
 	req.URL.Path = p
 
-	fmt.Printf("Request path: " + p)
-	cid, err := cid2.Decode(p)
+	sp := strings.Split(p, "/")
+	cid, err := cid2.Decode(sp[0])
+	nd, err := node.Get(c.Request().Context(), cid)
 
-	if err != nil {
-		panic(err)
-	}
-
-	rscDir, err := node.GetDirectoryWithCid(c.Request().Context(), cid)
-	if err != nil {
-		// 	check if file.
-		rscFile, err := node.GetFile(c.Request().Context(), cid)
-		content, err := io.ReadAll(rscFile)
-		c.Response().Write(content)
-		return nil
+	switch nd := nd.(type) {
+	case *merkledag.ProtoNode:
+		n, err := unixfs.FSNodeFromBytes(nd.Data())
 		if err != nil {
-			panic(err)
+			return err
 		}
+		if n.IsDir() {
+			return ServeDir(ctx, nd, c.Response().Writer, req)
+		}
+		if n.Type() == unixfs.TSymlink {
+			return fmt.Errorf("symlinks not supported")
+		}
+	case *merkledag.RawNode:
+	default:
+		return errors.New("unknown node type")
+	}
+	fmt.Println("serving unixfs", cid)
+	dr, err := uio.NewDagReader(ctx, nd, node.DAGService)
+	if err != nil {
+		return err
 	}
 
-	rscDir.GetNode()
+	err = SniffMimeType(c.Response().Writer, dr)
+	if err != nil {
+		return err
+	}
 
-	c.Response().Write([]byte("nice dir"))
+	http.ServeContent(c.Response().Writer, req, cid.String(), time.Time{}, dr)
+	return nil
+}
+
+func ServeDir(ctx context.Context, n mdagipld.Node, w http.ResponseWriter, req *http.Request) error {
+
+	dir, err := uio.NewDirectoryFromNode(node.DAGService, n)
+	if err != nil {
+		return err
+	}
+
+	nd, err := dir.Find(ctx, "index.html")
+	switch {
+	case err == nil:
+		dr, err := uio.NewDagReader(ctx, nd, node.DAGService)
+		if err != nil {
+			return err
+		}
+
+		http.ServeContent(w, req, "index.html", time.Time{}, dr)
+		return nil
+	default:
+		return err
+	case xerrors.Is(err, os.ErrNotExist):
+
+	}
+
+	fmt.Fprintf(w, "<html><body><ul>")
+
+	requestURI, err := url.ParseRequestURI(req.RequestURI)
+
+	if err := dir.ForEachLink(ctx, func(lnk *mdagipld.Link) error {
+		href := gopath.Join(requestURI.Path, lnk.Name)
+		fmt.Fprintf(w, "<li><a href=\"%s\">%s</a></li>", href, lnk.Name)
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "</ul></body></html>")
+	return nil
+}
+
+func SniffMimeType(w http.ResponseWriter, dr uio.DagReader) error {
+	// see kubo https://github.com/ipfs/kubo/blob/df222053856d3967ff0b4d6bc513bdb66ceedd6f/core/corehttp/gateway_handler_unixfs_file.go
+	// see http ServeContent https://cs.opensource.google/go/go/+/refs/tags/go1.19.2:src/net/http/fs.go;l=221;drc=1f068f0dc7bc997446a7aac44cfc70746ad918e0
+
+	// Calculate deterministic value for Content-Type HTTP header
+	// (we prefer to do it here, rather than using implicit sniffing in http.ServeContent)
+	var ctype string
+	// uses https://github.com/gabriel-vasile/mimetype library to determine the content type.
+	// Fixes https://github.com/ipfs/kubo/issues/7252
+	mimeType, err := mimetype.DetectReader(dr)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("cannot detect content-type: %s", err.Error()), http.StatusInternalServerError)
+		return err
+	}
+
+	ctype = mimeType.String()
+	_, err = dr.Seek(0, io.SeekStart)
+	if err != nil {
+		http.Error(w, "seeker can't seek", http.StatusInternalServerError)
+		return err
+	}
+	// Strip the encoding from the HTML Content-Type header and let the
+	// browser figure it out.
+	//
+	// Fixes https://github.com/ipfs/kubo/issues/2203
+	if strings.HasPrefix(ctype, "text/html;") {
+		ctype = "text/html"
+	}
+	// Setting explicit Content-Type to avoid mime-type sniffing on the client
+	// (unifies behavior across gateways and web browsers)
+	w.Header().Set("Content-Type", ctype)
 	return nil
 }
